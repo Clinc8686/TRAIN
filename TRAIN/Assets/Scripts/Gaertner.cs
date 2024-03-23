edd38e094713e8820c93d330fe82126ecd182817
Clinc8686 <mario1.lanpert@gmail.com>
2024-03-23 15:15:36 +0100

    starttext in gamescene3

---
+++ TRAIN/Assets/Scripts/Gaertner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Gaertner: MonoBehaviour, IInteractable 
+{
+    [SerializeField] private Player player;
+    [SerializeField] private Transform interactionSignTransform;
+    [SerializeField] private float interactionDistance = 1f;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite sprite;
+    
+    public void Interact()
+    {
+        
+        
+    }
+}

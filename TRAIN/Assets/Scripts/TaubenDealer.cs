ef4b25cbad701a6ef616355eb87cb4777b3322cb
Lulu <Luka.T@gmx.net>
2024-03-24 00:17:52 +0100

    Fickt euch alle, macht es selber, vorallem Melina!

--- TRAIN/Assets/Scripts/TaubenDealer.cs
+++ TRAIN/Assets/Scripts/TaubenDealer.cs
@@ -6,13 +6,34 @@ public class TaubenDealer : MonoBehaviour, IInteractable
 {
     [SerializeField] private Player player;
     [SerializeField] private Transform interactionSignTransform;
-    [SerializeField] private string dialogTextInput;
+    //[SerializeField] private string dialogTextInputWithSamen;
+    //[SerializeField] private string dialogTextInputWithOutSamen;
     [SerializeField] private float interactionDistance = 1f;
     [SerializeField] private CollectableSO collectableSO;
+    [SerializeField] private string[] textWithOutSamen;
+    [SerializeField] private string[] textWithSamen;
+
+    private int _index = 0;
+    private bool _hasFinishedJob;
+    private string[] text;
+    private void Start()
+    {
+        GameInputs.Instance.OnPlayerTextSkipped += PlayerInputSystem_GameInputs_OnPlayerTextSkipped;
+    }
+    private void PlayerInputSystem_GameInputs_OnPlayerTextSkipped(object sender, System.EventArgs e)
+    {
+        if (_hasFinishedJob) return;
+
+        if (_index >= text.Length - 1)
+        {
+            ResetState();
+        }
+
+        DialogController.Instance.ResetIsWritingState();
+        DialogController.Instance.WriteText(text[_index++]);
+    }
     private void Update()
     {
-        if (!InventoryController.Instance.HasSamen()) return;
-
         float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
         if (distanceToPlayer <= interactionDistance)
         {
@@ -23,15 +44,37 @@ public class TaubenDealer : MonoBehaviour, IInteractable
             if (interactionSignTransform.gameObject.activeInHierarchy)
                 interactionSignTransform.gameObject.SetActive(false);
         }
+
+        if (DialogController.Instance.IsWriting()) return;
+
+        if (_index >= text.Length - 1)
+        {
+            ResetState();
+        }
+
+        DialogController.Instance.WriteText(text[_index++]);
     }
     public void Interact()
     {
-        if (!InventoryController.Instance.HasSamen()) return;
-
+        _hasFinishedJob = false;
         float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
         if (distanceToPlayer > interactionDistance) return;
 
-        DialogController.Instance.WriteText(dialogTextInput);
-        InventoryController.Instance.AddNewElementToInventory(collectableSO);
+        if (!InventoryController.Instance.HasSamen())
+        {
+            //DialogController.Instance.WriteText(dialogTextInputWithOutSamen);
+            text = textWithOutSamen;
+        }
+        else
+        {
+            text = textWithSamen;
+            //DialogController.Instance.WriteText(dialogTextInputWithSamen);
+            InventoryController.Instance.AddNewElementToInventory(collectableSO);
+        }
+    }
+    private void ResetState()
+    {
+        DialogController.Instance.ResetWritingStateAndDeactivateUI();
+        _hasFinishedJob = true;
     }
 }
